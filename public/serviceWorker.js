1cc6a3fcf71b3b869fe6a39d3397bae991b61bc9
Mtkn573 <mtkn573@proton.me>
2025-06-28 14:51:47 +0200

    Add requireInteraction.

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,6 +1,7 @@
 self.addEventListener('push', function (event) {
   data = event.data.json()
   self.registration.showNotification(data.title, {
-    body: data.body
+    body: data.body,
+    requireInteraction: true
   })
 })
