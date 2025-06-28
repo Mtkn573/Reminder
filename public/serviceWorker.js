439af1ad5180795bb8004ee2b588f91b62180a21
Mtkn573 <mtkn573@proton.me>
2025-06-28 15:14:45 +0200

    I guess add audio.
    I guess remove requireInteraction.

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,6 @@
 self.addEventListener('push', function (event) {
   data = event.data.json()
   self.registration.showNotification(data.title, {
-    body: data.body,
-    requireInteraction: true
+    body: data.body
   })
 })
